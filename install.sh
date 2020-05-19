1548ef6611ede3b4e308bc52715b303dc16a2295
Billy Watson <billy.watson@disney.com>
2020-05-18 23:29:45 -0400

    try pip installs without sudo

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 sudo apt-get -y upgrade
 sudo apt-get install -y python3 python3-pip 
 sudo apt-get install -y libsdl1.2-dev libsdl-mixer1.2
-sudo pip3 install rpi_ws281x adafruit-circuitpython-neopixel
+pip3 install rpi_ws281x adafruit-circuitpython-neopixel
 sudo apt-get install -y python-smbus
-sudo pip3 install --upgrade setuptools
-sudo pip3 install RPI.GPIO
-sudo pip3 install PySDL2 pygame nfcpy
+pip3 install --upgrade setuptools
+pip3 install RPI.GPIO
+pip3 install PySDL2 pygame nfcpy
 amixer set PCM 100%
